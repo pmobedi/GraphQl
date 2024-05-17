4a3a7e39b359d5ba214bd5da4b4158940b402080
mobedi <p.mobedi@gmail.com>
2024-05-17 07:25:38 +0330

    setup graphql

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const app = express();
 
+app.use('/graphql', graphqlHTTP({
+
+}))
 app.listen(3000, () => {console.log('server run on port 3000 ...')});
