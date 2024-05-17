004550137f958e5193ac152140836e99608af16f
mobedi <p.mobedi@gmail.com>
2024-05-17 08:42:45 +0330

    Arguments

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ mongoose.connect('mongodb://127.0.0.1/graphql-project');
 let schema = buildSchema(`
     type Query {
         user : User
+        getAllUser : [User]
+        getUser(id : ID) : User
         FakeData : String
     }
     type User {
@@ -65,20 +67,29 @@ let resolver = {
         }
     },
     FakeData : () => {
-       
+        for(let i =0 ; i<=10 ;  i++){
             const addUser = new User({
                 fname : faker.name.firstName(),
                 lname : faker.name.lastName(),
-                 age : faker.random.number(),
+                age : faker.random.number(),
                 email : faker.internet.email(),
                 password : faker.internet.password() ,      
              
+               // title : faker.lorem.sentence(),
+               // body : faker.lorem.text()
             })
             addUser.save();
-        
+        }
         return "data store ..."
     },
-
+    getAllUser : async () => {
+        const users = await  User.find({});
+        return users;
+    },
+    getUser : async (args) => {
+        const user = await User.findById(args.id)
+        return user;
+    }
 }
 app.use('/graphql', graphqlHTTP({
    schema : schema,
