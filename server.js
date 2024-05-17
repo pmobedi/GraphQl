aeb6f8c0897cb6155927689677611120255084b7
mobedi <p.mobedi@gmail.com>
2024-05-17 07:31:56 +0330

    operation type query

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const { buildSchema } = require('graphql');
+
 const app = express();
+let schema = buildSchema(`
+    schema {
+        query : rootQuery
+    }
+    type rootQuery {
+        user : String
+    }
+`)
 
 app.use('/graphql', graphqlHTTP({
 
+    schema : schema,
 }))
 app.listen(3000, () => {console.log('server run on port 3000 ...')});
